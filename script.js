dfb434a21fb5d817c6c482d950a307cdac6a8924
Sabra <janfazasabra@gmail.com>
2022-12-24 04:07:39 +0330

    Search,Current Button functions added

--- script.js
+++ script.js
@@ -93,4 +93,4 @@ searchForm.addEventListener("submit", citySubmit);
 let currentLocationButton = document.querySelector("#current-location-button");
 currentLocationButton.addEventListener("click", getCurrentLocation);
 
-searchCity("London2");
+searchCity("paris");
